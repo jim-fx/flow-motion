2d924e53448b5e3f1e158d9f387c8c75fe2d737e
karkem <karlkemper@web.de>
2023-12-31 16:16:35 +0100

    do some shit

--- ble_server/ble_server.ino
+++ ble_server/ble_server.ino
@@ -3,9 +3,9 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include <Wire.h>
-#include "MPU6050.h"
+#include "MPU6050_6Axis_MotionApps20.h"
 
-//BLE server name
+// BLE server name
 #define bleServerName "ESP32"
 
 int16_t ax, ay, az;
@@ -17,38 +17,115 @@ bool deviceConnected = false;
 // https://www.uuidgenerator.net/
 #define SERVICE_UUID "91bad492-b950-4226-aa2b-4ede9fa42f59"
 
-MPU6050 accelgyro;
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+
+MPU6050 mpu;
 BLEServer *pServer;
 BLECharacteristic bmeTemperatureCelsiusCharacteristics("cba1d466-344c-4be3-ab3f-189f80dd7518", BLECharacteristic::PROPERTY_NOTIFY);
 BLEDescriptor bmeTemperatureCelsiusDescriptor(BLEUUID((uint16_t)0x2902));
 
-//Setup callbacks onConnect and onDisconnect
-class MyServerCallbacks: public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+int16_t ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+// Setup callbacks onConnect and onDisconnect
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
     deviceConnected = true;
     Serial.println("Device Connected");
   };
-  void onDisconnect(BLEServer* pServer) {
+  void onDisconnect(BLEServer *pServer)
+  {
     deviceConnected = false;
     Serial.println("Device Disconnected");
   }
 };
 
+void setup_mpu(){
 
-void setup() {
-  // Start serial communication 
+   // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+    
+    delay(2000);
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        Serial.println(F(")..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+}
+
+void setup()
+{
+  // Start serial communication
   Serial.begin(115200);
-  
+
   // setup software i2c
   Wire.begin();
-  
-  // initialize device
-  Serial.println("Initializing I2C devices...");
-  accelgyro.initialize();
 
-  // verify connection
-  Serial.println("Testing device connections...");
-  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+  setup_mpu();
 
   // Create the BLE Device
   BLEDevice::init(bleServerName);
@@ -64,7 +141,7 @@ void setup() {
   bmeService->addCharacteristic(&bmeTemperatureCelsiusCharacteristics);
   bmeTemperatureCelsiusDescriptor.setValue("BME temperature Celsius");
   bmeTemperatureCelsiusCharacteristics.addDescriptor(&bmeTemperatureCelsiusDescriptor);
-  
+
   // Start the service
   bmeService->start();
 
@@ -77,24 +154,57 @@ void setup() {
 
 int idx = 0;
 
-void serialize_int16_t(uint8_t* buffer, int16_t* values, size_t count) {
-    for (size_t i = 0; i < count; ++i) {
-        buffer[i * 2] = (uint8_t)(values[i] & 0xFF);
-        buffer[i * 2 + 1] = (uint8_t)((values[i] >> 8) & 0xFF);
-    }
+void serialize_int16_t(uint8_t *buffer, int16_t *values, size_t count)
+{
+  for (size_t i = 0; i < count; ++i)
+  {
+    buffer[i * 2] = (uint8_t)(values[i] & 0xFF);
+    buffer[i * 2 + 1] = (uint8_t)((values[i] >> 8) & 0xFF);
+  }
 }
 
-
-void loop() {
+void loop()
+{
 
   idx++;
 
-  if (deviceConnected) {
+  if (deviceConnected)
+  {
+
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer))
+    { // Get the Latest packet
+      // display yaw pitch roll in degrees
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+
+      for (int i = 0; i < 3; i++)
+      {
+        ypr[i] = (ypr[i] * 180 / M_PI);
+        if (ypr[i] > 90 && ypr[i] <= 180)
+        {
+          ypr[i] = 90 - (ypr[i] - 90);
+        }
+        if (ypr[i] < -90 && ypr[i] >= -180)
+        {
+          ypr[i] = -90 - (ypr[i] + 90);
+        }
+        ypr[i] = ypr[i] / 10.0;
+      }
+      Serial.print("aworld\t");
+      Serial.print(aaWorld.x);
+      Serial.print("\t");
+      Serial.print(aaWorld.y);
+      Serial.print("\t");
+      Serial.println(aaWorld.z);
+    }
 
     // read raw accel/gyro measurements from device
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz); 
+    //accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-    int16_t values[] = {ax, ay, az, gx, gy, gz};
+    int16_t values[] = {aaWorld.x, aaWorld.y, aaWorld.z, ypr[0], ypr[1], ypr[2]};
     size_t count = sizeof(values) / sizeof(values[0]);
 
     // Serialize
@@ -104,7 +214,8 @@ void loop() {
     // Set characteristic value and notify connected client
     bmeTemperatureCelsiusCharacteristics.setValue(serializedBuffer, count * 2);
     bmeTemperatureCelsiusCharacteristics.notify();
-    if (idx % 20 == 0 && false) {
+    if (idx % 20 == 0 && false)
+    {
       // display tab-separated accel/gyro x/y/z values
       Serial.print("a/g:\t");
       Serial.print(ax);
@@ -119,17 +230,18 @@ void loop() {
       Serial.print("\t");
       Serial.println(gz);
     }
-
   }
 
-    // Check for a reset command from serial
-  if (Serial.available() > 0) {
+  // Check for a reset command from serial
+  if (Serial.available() > 0)
+  {
     char command = Serial.read();
 
     // Check if the received command is 'R'
-    if (command == 'R' || command == 'r') {
+    if (command == 'R' || command == 'r')
+    {
       Serial.println("Resetting...");
-      delay(1000); // Optional delay for visibility in the Serial Monitor
+      delay(1000);   // Optional delay for visibility in the Serial Monitor
       ESP.restart(); // This function resets the ESP32
     }
   }
--- esp32-c3.ino
+++ esp32-c3.ino
@@ -2,19 +2,44 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
-#include "MPU6050.h"
+//#include "MPU6050.h"
 #include "Wire.h"
+#include "MPU6050_6Axis_MotionApps20.h"
 
 #define SERVICE_UUID "9b9f77c6-7e68-4109-b987-b096233d9525"
 #define CHARACTERISTIC_UUID "1ab2c9f4-19c0-48dd-8932-ed72558ec593"
 
-MPU6050 accelgyro;
+MPU6050 mpu;
 BLECharacteristic *pCharacteristic;
 BLEServer *pServer;
 bool deviceConnected = false;
 
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
+//int16_t ax, ay, az;
+//int16_t gx, gy, gz;
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+int16_t ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
 
 class MyServerCallbacks : public BLEServerCallbacks {
   void onConnect(BLEServer *pServer) {
@@ -60,99 +85,124 @@ void setup() {
   // it's really up to you depending on your project)
   Serial.begin(115200);
 
-  // initialize device
-  Serial.println("Initializing I2C devices...");
-  accelgyro.initialize();
-
-  // verify connection
-  Serial.println("Testing device connections...");
-  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-
-  // use the code below to change accel/gyro offset values
-  Serial.println("Updating internal sensor offsets...");
-  // -76	-2359	1688	0	0	0
-  Serial.print(accelgyro.getXAccelOffset());
-  Serial.print("\t");  // -76
-  Serial.print(accelgyro.getYAccelOffset());
-  Serial.print("\t");  // -2359
-  Serial.print(accelgyro.getZAccelOffset());
-  Serial.print("\t");  // 1688
-  Serial.print(accelgyro.getXGyroOffset());
-  Serial.print("\t");  // 0
-  Serial.print(accelgyro.getYGyroOffset());
-  Serial.print("\t");  // 0
-  Serial.print(accelgyro.getZGyroOffset());
-  Serial.print("\t");  // 0
-  Serial.print("\n");
-  accelgyro.setXGyroOffset(220);
-  accelgyro.setYGyroOffset(76);
-  accelgyro.setZGyroOffset(-85);
-  Serial.print(accelgyro.getXAccelOffset());
-  Serial.print("\t");  // -76
-  Serial.print(accelgyro.getYAccelOffset());
-  Serial.print("\t");  // -2359
-  Serial.print(accelgyro.getZAccelOffset());
-  Serial.print("\t");  // 1688
-  Serial.print(accelgyro.getXGyroOffset());
-  Serial.print("\t");  // 0
-  Serial.print(accelgyro.getYGyroOffset());
-  Serial.print("\t");  // 0
-  Serial.print(accelgyro.getZGyroOffset());
-  Serial.print("\t");  // 0
-  Serial.print("\n");
-
-
   setup_ble();
+
+  // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+    
+    delay(2000);
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        Serial.println(F(")..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
 }
 
 void loop() {
-  // read raw accel/gyro measurements from device
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-  // these methods (and a few others) are also available
-  //accelgyro.getAcceleration(&ax, &ay, &az);
-  //accelgyro.getRotation(&gx, &gy, &gz);
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+      // display yaw pitch roll in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
 
-  // display tab-separated accel/gyro x/y/z values
-  Serial.print("a/g:\t");
-  Serial.print(ax);
-  Serial.print("\t");
-  Serial.print(ay);
-  Serial.print("\t");
-  Serial.print(az);
-  Serial.print("\t");
-  Serial.print(gx);
-  Serial.print("\t");
-  Serial.print(gy);
-  Serial.print("\t");
-  Serial.println(gz);
+            for(int i = 0; i < 3; i++)
+      {
+        ypr[i] = (ypr[i] * 180/M_PI);
+        if (ypr[i] > 90 && ypr[i] <= 180)
+        {
+          ypr[i] = 90-(ypr[i]-90);
+        }
+        if (ypr[i] < -90 && ypr[i] >= -180)
+        {
+          ypr[i] = -90-(ypr[i]+90);
+        }
+        ypr[i] = ypr[i]/10.0;
+      }
+          Serial.print("aworld\t");
+    Serial.print(aaWorld.x);
+    Serial.print("\t");
+    Serial.print(aaWorld.y);
+    Serial.print("\t");
+    Serial.println(aaWorld.z);
+  }
+
+    int16_t Roll = ypr[0];
+    int16_t Pitch = ypr[1];
+    int16_t Yaw = ypr[2];
 
   if (deviceConnected) {
 
     uint8_t encodedData[12];
 
-    encodedData[0] = static_cast<uint8_t>(ax & 0xFF);
-    encodedData[1] = static_cast<uint8_t>((ax >> 8) & 0xFF);
+    encodedData[0] = static_cast<uint8_t>(aaWorld.x & 0xFF);
+    encodedData[1] = static_cast<uint8_t>((aaWorld.x >> 8) & 0xFF);
 
-    encodedData[2] = static_cast<uint8_t>(ay & 0xFF);
-    encodedData[3] = static_cast<uint8_t>((ay >> 8) & 0xFF);
+    encodedData[2] = static_cast<uint8_t>(aaWorld.y & 0xFF);
+    encodedData[3] = static_cast<uint8_t>((aaWorld.y >> 8) & 0xFF);
 
-    encodedData[4] = static_cast<uint8_t>(az & 0xFF);
-    encodedData[5] = static_cast<uint8_t>((az >> 8) & 0xFF);
+    encodedData[4] = static_cast<uint8_t>(aaWorld.z & 0xFF);
+    encodedData[5] = static_cast<uint8_t>((aaWorld.z >> 8) & 0xFF);
 
-    encodedData[6] = static_cast<uint8_t>(gx & 0xFF);
-    encodedData[7] = static_cast<uint8_t>((gx >> 8) & 0xFF);
+    encodedData[6] = static_cast<uint8_t>(Roll & 0xFF);
+    encodedData[7] = static_cast<uint8_t>((Roll >> 8) & 0xFF);
 
-    encodedData[8] = static_cast<uint8_t>(gy & 0xFF);
-    encodedData[9] = static_cast<uint8_t>((gy >> 8) & 0xFF);
+    encodedData[8] = static_cast<uint8_t>(Pitch & 0xFF);
+    encodedData[9] = static_cast<uint8_t>((Pitch >> 8) & 0xFF);
 
-    encodedData[10] = static_cast<uint8_t>(gz & 0xFF);
-    encodedData[11] = static_cast<uint8_t>((gz >> 8) & 0xFF);
+    encodedData[10] = static_cast<uint8_t>(Yaw & 0xFF);
+    encodedData[11] = static_cast<uint8_t>((Yaw >> 8) & 0xFF);
 
     pCharacteristic->setValue(encodedData, sizeof(encodedData));
     pCharacteristic->notify();
-  }
 
+    Serial.println("KLSJLSJDLKSDKL");
+  }
 
   delay(50);
 }
